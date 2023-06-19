abe714496b7fec2a0ce68082e4d650591ce4497b
xQuartyx <95701206+xQuartyx@users.noreply.github.com>
2023-06-19 20:06:23 +0700

    Update Library.lua

--- LinoriaLib/Library.lua
+++ LinoriaLib/Library.lua
@@ -2962,7 +2962,7 @@ function Library:CreateWindow(...)
             if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
                 task.spawn(Library.Toggle)
             end
-        elseif Input.KeyCode == Enum.KeyCode.RightControl or (Input.KeyCode == Enum.KeyCode.RightShift and (not Processed)) then
+        elseif Input.KeyCode == Enum.KeyCode.RightControl or (_G.Keybind and Input.KeyCode.Name == _G.Keybind) or (Input.KeyCode == Enum.KeyCode.RightShift and (not Processed)) then
             task.spawn(Library.Toggle)
         end
 
