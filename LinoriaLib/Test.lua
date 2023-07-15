057f0d7fd18203a25cb45fd8eab249c5536fd3df
xQuartyx <95701206+xQuartyx@users.noreply.github.com>
2023-07-15 15:03:42 +0700

    Update Test.lua

--- LinoriaLib/Test.lua
+++ LinoriaLib/Test.lua
@@ -3545,45 +3545,45 @@ function Library:CreateWindow(...)
             -- A bit scuffed, but if we're going from not toggled -> toggled we want to show the frame immediately so that the fade is visible.
             Outer.Visible = true;
 
-            task.spawn(function()
-                -- TODO: add cursor fade?
-                local State = InputService.MouseIconEnabled;
+        --     task.spawn(function()
+        --         -- TODO: add cursor fade?
+        --         local State = InputService.MouseIconEnabled;
 
-                local Cursor = Drawing.new('Triangle');
-                Cursor.Thickness = 1;
-                Cursor.Filled = true;
-                Cursor.Visible = true;
+        --         local Cursor = Drawing.new('Triangle');
+        --         Cursor.Thickness = 1;
+        --         Cursor.Filled = true;
+        --         Cursor.Visible = true;
 
-                local CursorOutline = Drawing.new('Triangle');
-                CursorOutline.Thickness = 1;
-                CursorOutline.Filled = false;
-                CursorOutline.Color = Color3.new(0, 0, 0);
-                CursorOutline.Visible = true;
+        --         local CursorOutline = Drawing.new('Triangle');
+        --         CursorOutline.Thickness = 1;
+        --         CursorOutline.Filled = false;
+        --         CursorOutline.Color = Color3.new(0, 0, 0);
+        --         CursorOutline.Visible = true;
 
-                while Toggled and ScreenGui.Parent do
-                    InputService.MouseIconEnabled = false;
+        --         while Toggled and ScreenGui.Parent do
+        --             InputService.MouseIconEnabled = false;
 
-                    local mPos = InputService:GetMouseLocation();
+        --             local mPos = InputService:GetMouseLocation();
 
-                    Cursor.Color = Library.AccentColor;
+        --             Cursor.Color = Library.AccentColor;
 
-                    Cursor.PointA = Vector2.new(mPos.X, mPos.Y);
-                    Cursor.PointB = Vector2.new(mPos.X + 16, mPos.Y + 6);
-                    Cursor.PointC = Vector2.new(mPos.X + 6, mPos.Y + 16);
+        --             Cursor.PointA = Vector2.new(mPos.X, mPos.Y);
+        --             Cursor.PointB = Vector2.new(mPos.X + 16, mPos.Y + 6);
+        --             Cursor.PointC = Vector2.new(mPos.X + 6, mPos.Y + 16);
 
-                    CursorOutline.PointA = Cursor.PointA;
-                    CursorOutline.PointB = Cursor.PointB;
-                    CursorOutline.PointC = Cursor.PointC;
+        --             CursorOutline.PointA = Cursor.PointA;
+        --             CursorOutline.PointB = Cursor.PointB;
+        --             CursorOutline.PointC = Cursor.PointC;
 
-                    RenderStepped:Wait();
-                end;
+        --             RenderStepped:Wait();
+        --         end;
 
-                InputService.MouseIconEnabled = State;
+        --         InputService.MouseIconEnabled = State;
 
-                Cursor:Remove();
-                CursorOutline:Remove();
-            end);
-        end;
+        --         Cursor:Remove();
+        --         CursorOutline:Remove();
+        --     end);
+        -- end;
 
         for _, Desc in next, Outer:GetDescendants() do
             local Properties = {};
